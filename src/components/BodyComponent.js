0170adedd47444e42b6721070595d806f6075416
Vaibhav Singh <vaibhavsinghr.08@gmail.com>
2023-09-15 16:48:24 +0530

    feat: search restaurant

--- src/components/BodyComponent.js
+++ src/components/BodyComponent.js
@@ -3,49 +3,80 @@ import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 
 const BodyComponent = () => {
-
   // local State variable
-  const [resObjArray, setResObjArray] = useState([
-  ]);
+  const [resObjArray, setResObjArray] = useState([]);
+
+  // local State variable for search text
+  const [searchText, setSearchText] = useState("");
+
+  console.log("Body Component Rendered");
 
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    const response = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6613353&lng=77.22749449999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const response = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6613353&lng=77.22749449999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
     const jsonData = await response.json();
 
     console.log(jsonData);
     //optional chaining
-    setResObjArray(jsonData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setResObjArray(
+      jsonData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
   };
 
   // Conditional Rendering
 
-  return resObjArray.length === 0 ? (<Shimmer />) : (
+  return resObjArray.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn"
+        <button
+          className="filter-btn"
           onClick={() => {
             const filterRestaurants = resObjArray.filter(
               (res) => res.info.avgRating > 4.0
             );
             setResObjArray(filterRestaurants);
-          }}>Top Restaurants</button>
+          }}
+        >
+          Top Restaurants
+        </button>
       </div>
       <div className="search-bar">
-        <input type="text" placeholder="Search for restaurants" />
-        <button type="button" className="search-button">Search</button>
+        <input
+          type="text"
+          placeholder="Search for restaurants"
+          className="search-box"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+
+        <button
+          type="button"
+          className="search-button"
+          onClick={() => {
+            const restaurantSearch = resObjArray.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setResObjArray(restaurantSearch);
+          }}
+        >
+          Search
+        </button>
       </div>
       <div className="restaurant-container">
-        {
-          resObjArray.map((resObj) => (
-            < RestaurantCardComponent key={resObj.info.id} resData={resObj} />
-          ))
-        }
+        {resObjArray.map((resObj) => (
+          <RestaurantCardComponent key={resObj.info.id} resData={resObj} />
+        ))}
       </div>
-    </div>
+    </div >
   );
 };
 
