fedc40956d1c5c6f8016a084849a428a70e65f12
Vaibhav Singh <vaibhavsinghr.08@gmail.com>
2023-09-18 20:30:13 +0530

    feat: added routes

--- src/app.js
+++ src/app.js
@@ -3,16 +3,44 @@ import ReactDOM from "react-dom/client";
 import HeaderComponent from "./components/HeaderComponent";
 import BodyComponent from "./components/BodyComponent";
 import FooterComponent from "./components/FooterComponent";
+import ErrorComponent from "./components/ErrorComponent";
+import AboutComponent from "./components/AboutComponent";
+import CartComponent from "./components/CartComponent";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <div className="app">
       <HeaderComponent />
-      <BodyComponent />
+      <Outlet />
       <FooterComponent />
     </ div>
   );
 };
 
+const appRoute = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <BodyComponent />,
+        },
+        {
+          path: "/about",
+          element: <AboutComponent />,
+        },
+        {
+          path: "/cart",
+          element: <CartComponent />,
+        },
+      ],
+      errorElement: <ErrorComponent />,
+    }
+  ]
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRoute} />);
