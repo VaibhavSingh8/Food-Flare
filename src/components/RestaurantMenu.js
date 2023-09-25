84f9e4c5de68bd6319bbf2c4d5dfa803c1ea4e19
Vaibhav Singh <vaibhavsinghr.08@gmail.com>
2023-09-25 10:32:11 +0530

    refactored using custom hook

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,25 +1,12 @@
-import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import { MENU_API } from "../utils/constants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
 
   const { resId } = useParams();
 
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-
-  const fetchMenu = async () => {
-    const apiResponse = await fetch(
-      MENU_API + resId
-    );
-    const jsonResponse = await apiResponse.json();
-
-    setResInfo(jsonResponse.data);
-  };
+  const resInfo = useRestaurantMenu(resId);
 
   if (resInfo === null) return <Shimmer />;
 
