597d4f41463371f32b4ee0e27c9feefc0bdbf49d
Vaibhav Singh <vaibhavsinghr.08@gmail.com>
2023-12-24 23:07:57 +0530

    fix: CORS issue

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,4 @@ export const IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 
 export const LOGO_URL = "https://w7.pngwing.com/pngs/894/279/png-transparent-online-food-ordering-food-delivery-grubhub-others-food-service-logo-thumbnail.png";
 
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.61129608341754&lng=77.44435027241707&restaurantId="
+export const MENU_API = "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.61129608341754&lng=77.44435027241707&restaurantId="
